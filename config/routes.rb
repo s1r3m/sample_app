56739c2a0726809378b0bb0cf420f5f11504af59
xelnaga <falexandrov@gmail.com>
2013-07-31 11:53:41 +0000

    Add about_page, titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 SampleApp::Application.routes.draw do
+  
   get "static_pages/home"
-
-  get "static_pages/homee"
-
+  get "static_pages/about"
   get "static_pages/help"
 
   # The priority is based upon order of creation:
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+    # Home page
+    describe "Home page" do
+        # <h1>
+        it "should have the content 'Sample app'" do
+            visit '/static_pages/home'
+            page.should have_selector('h1', :text => "Sample App")
+        end
+        # right title
+        it "should have the right title" do
+            visit '/static_pages/home'
+            page.should have_selector('title', :text => " | Home")
+        end
+    end
+
+    # Help page
+    describe "Help page" do
+        it "should have the content 'Help'" do
+            visit '/static_pages/help'
+            page.should have_selector('h1', :text => "Help")
+        end
+         # right title
+        it "should have the right title" do
+            visit '/static_pages/help'
+            page.should have_selector('title', :text => " | Help")
+        end
+    end
+    
+    # About page
+    describe "About page" do
+        it "should have content 'About Us'" do
+            visit '/static_pages/about'
+            page.should have_selector('h1', :text => "About Us")
+        end
+         # right title
+        it "should have the right title" do
+            visit '/static_pages/about'
+            page.should have_selector('title', :text => " | About")
+        end
+    end
+end
